0c381b1579af1b06a91eefa6c68372150c2d6bb5
cshi <cshi@yahoo-inc.com>
2017-10-18 15:04:26 -0700

    introducing QUnit

---
+++ tests/unit/class.js
@@ -0,0 +1,14 @@
+// This is the javascript file for unit test
+
+// Sample test that qunit is working
+
+QUnit.test('qunit valid', function( assert ) {
+  assert.ok(1 == "1", "Passed!" );
+});
+
+QUnit.module('class', function () {
+  QUnit.test('Class exists', function (assert) {
+    assert.ok(Class, "class existence passed");
+  });
+});
+
